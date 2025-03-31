fd37ecc5de54a0279a0d9c594ee5ff61cc982119
Kevin Wittek <kevin@wittek.dev>
2025-03-31 17:29:30 +0200

    Add ModelRunnerContainer pulling behavior

---
+++ README.md
@@ -0,0 +1,4 @@
+# GenAI Development with Testcontainers and Docker Model Runner
+
+# License
+This project is licensed under the MIT License. See the LICENSE file for details.
--- src/main/java/dev/wittek/tcgenai/ModelRunnerContainer.java
+++ src/main/java/dev/wittek/tcgenai/ModelRunnerContainer.java
@@ -3,6 +3,12 @@ package dev.wittek.tcgenai;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.SocatContainer;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ModelRunnerContainer extends GenericContainer {
 
     public static final String MODEL_RUNNER_ENDPOINT = "model-runner.docker.internal";
@@ -11,11 +17,28 @@ public class ModelRunnerContainer extends GenericContainer {
 
     @Override
     public void start() {
-        logger().debug("Pulling model: {}", model);
-
         socat = new SocatContainer()
                 .withTarget(80, MODEL_RUNNER_ENDPOINT, 80);
         socat.start();
+
+        logger().info("Pulling model: {}. Please be patient, no progress bar yet!", model);
+        try {
+            // Construct JSON payload
+            String json = String.format("{\"from\":\"%s\"}", model);
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://" + socat.getHost() + ":" + socat.getMappedPort(80) + "/models/create"))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(json))
+                    .build();
+            HttpResponse<String> response =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            logger().info(response.body());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        logger().info("Finished pulling model: {}", model);
     }
 
     @Override
